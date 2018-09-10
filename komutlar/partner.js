05058e2f18df97f3b074db81fe6439ee73bdca41
PlayWithFireTR <43099320+PlayWithFireTR@users.noreply.github.com>
2018-09-10 20:10:01 +0300

    Add files via upload

---
+++ komutlar/partner.js
@@ -0,0 +1,31 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+exports.run = (client, message) => {
+  if (message.channel.type !== 'dm') {
+    const ozelmesajkontrol = new Discord.RichEmbed()
+    .setColor(0x00AE86)
+    .setTimestamp()
+    .setAuthor(message.author.username, message.author.avatarURL)
+    .setDescription('Ozel mesajlarini kontrol et. :postbox:');
+    message.channel.sendEmbed(ozelmesajkontrol) }
+	const pingozel = new Discord.RichEmbed()
+    .setColor(0x00AE86)
+    .setTimestamp()
+    .setAuthor(message.author.username, message.author.avatarURL)
+    .setDescription('Iste Davet Linkim: https://discordapp.com/oauth2/authorize?client_id=475725086557732864&scope=bot&permissions=805314622 Buda Birkac Partner Sunucum  https://discord.gg/s5qNPWP');
+    return message.author.sendEmbed(pingozel)
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: ['botu ekle', 'botu davet et', 'botuekle', 'invite'],
+  permLevel: 0
+};
+
+exports.help = {
+  name: 'partner',
+  description: 'Birkaç Sunucu Gönderir.',
+  usage: 'partner'
+};
